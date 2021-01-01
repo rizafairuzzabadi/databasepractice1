71afb064030860b490634909bfa724d272e7af37
Riza <ed.fairuzz@gmail.com>
2021-01-01 16:38:14 +0700

    Update1jan

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
 from datetime import datetime
+
 import view
+from database import Database
+from movie import Movie
+
 
 def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", view_func=view.home_page)
-    app.add_url_rule("/movies", view_func=view.movies_page)
+    app.add_url_rule("/movies", view_func=view.movies_page,
+                     methods=["GET", "POST"])
+    app.add_url_rule("/new_movie", view_func=view.movie_add_page,
+                     methods=["GET", "POST"])
+    app.add_url_rule("/movies/<int:movie_key>",
+                     view_func=view.movie_page)
+    db = Database()
+    db.add_movie(Movie("Slaughterhouse-Five", year=1972))
+    db.add_movie(Movie("The Shining"))
+    app.config["db"] = db
+
     app.config.from_object("settings")
     #app.config["DEBUG"] = True
     return app
 
+
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
--- view.py
+++ view.py
@@ -1,10 +1,100 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app, abort, request, redirect, url_for
 from datetime import datetime
+from movie import Movie
+
 
 def home_page():
-    today=datetime.today()
+    today = datetime.today()
     day_name = today.strftime("%A")
     return render_template("home.html", day=day_name)
 
+
 def movies_page():
-    return render_template("movies.html")
+    db = current_app.config["db"]
+    if request.method == "GET":
+        movies = db.get_movies()
+        return render_template("movies.html", movies=sorted(movies))
+    else:
+        form_movie_keys = request.form.getlist("movie_keys")
+        for form_movie_key in form_movie_keys:
+            db.delete_movie(int(form_movie_key))
+        return redirect(url_for("movies_page"))
+
+
+def movie_page(movie_key):
+    db = current_app.config["db"]
+    movie = db.get_movie(movie_key)
+    if movie is None:
+        abort(404)
+    return render_template("movie.html", movie=movie)
+
+
+def movie_add_page():
+    if request.method == "GET":
+        values = {"title": "", "year": ""}
+        return render_template("movie_edit.html", min_year=1887, max_year=datetime.now().year, values=values)
+    else:
+        valid = validate_movie_form(request.form)
+        if not valid:
+            return render_template("movie_edit.html", min_year=1887, max_year=datetime.now().year, values=request.form)
+        form_title = request.form["title"]
+        form_year = request.form["year"]
+        movie = Movie(form_title, year=int(form_year) if form_year else None)
+        db = current_app.config["db"]
+        movie_key = db.add_movie(movie)
+        return redirect(url_for("movie_page", movie_key=movie_key))
+
+
+def validate_movie_form(form):
+    form.data = {}
+    form.errors = {}
+
+    form_title = form.get("title", "").strip()
+    if len(form_title) == 0:
+        form.errors["title"] = "Title can not be blank."
+    else:
+        form.data["title"] = form_title
+
+    form_year = form.get("year")
+    if not form_year:
+        form.data["year"] = None
+    elif not form_year.isdigit():
+        form.errors["year"] = "Year must consist of digits only."
+    else:
+        year = int(form_year)
+        if (year < 1887) or (year > datetime.now().year):
+            form.errors["year"] = "Year not in valid range."
+        else:
+            form.data["year"] = year
+
+    return len(form.errors) == 0
+
+
+def movie_edit_page(movie_key):
+    if request.method == "GET":
+        db = current_app.config["db"]
+        movie = db.get_movie(movie_key)
+        if movie is None:
+            abort(404)
+        values = {"title": movie.title, "year": movie.year}
+        return render_template(
+            "movie_edit.html",
+            min_year=1887,
+            max_year=datetime.now().year,
+            values=values,
+        )
+    else:
+        valid = validate_movie_form(request.form)
+        if not valid:
+            return render_template(
+                "movie_edit.html",
+                min_year=1887,
+                max_year=datetime.now().year,
+                values=request.form,
+            )
+        title = request.form.data["title"]
+        year = request.form.data["year"]
+        movie = Movie(title, year=year)
+        db = current_app.config["db"]
+        db.update_movie(movie_key, movie)
+        return redirect(url_for("movie_page", movie_key=movie_key))
