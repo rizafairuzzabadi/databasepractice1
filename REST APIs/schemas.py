8c97d260de6a21d36aeb19d94cecbb40000b353c
rizafairuzzabadi <fairuzzabadi17@itu.edu.tr>
2022-12-14 01:53:53 +0300

    Update schemas.py

--- REST APIs/schemas.py
+++ REST APIs/schemas.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 
 class ItemSchema(Schema):
     id = fields.Str(dump_only=True) # dump_only=True means that this field will not be required when creating a new item
-    name = fields.Str(dump_only=True) # dump_only=True means that this field will not be required when creating a new item
+    name = fields.Str(required=True) # required=True means that this field is required when creating a new item
     price = fields.Float(required=True) # required=True means that this field is required when creating a new item
     store_id = fields.Str(required=True) # required=True means that this field is required when creating a new item
 
